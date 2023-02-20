6b40fb79fc86516e2e1d5377f474e2d62de7ad14
Chalo1996 <emusyoka759@gmail.com>
2023-02-20 11:40:36 +0300

    off-set pagination indexes

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,6 @@
 """0-simple_helper_function module."""
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index and an end index."""
     return ((page - 1) * page_size, (page * page_size))
