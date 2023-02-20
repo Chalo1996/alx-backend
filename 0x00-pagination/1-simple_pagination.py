6bab51a09b09fe47e8a734c550feeef7e5b1bb25
Chalo1996 <emusyoka759@gmail.com>
2023-02-20 12:36:11 +0300

    cached dataset

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""0-simple_helper_function module."""
+
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple of size two containing a start index and an end index."""
+    return ((page - 1) * page_size, (page * page_size))
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset (i.e. the correct list\
+            of rows)."""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        if page > math.ceil(len(self.dataset()) / page_size):
+            return []
+        start, end = index_range(page, page_size)
+        return self.__dataset[start:end]
