91a479f69a0d3dc7a66ca0f8afdc79f78e3a5011
Chalo1996 <emusyoka759@gmail.com>
2023-02-21 17:05:41 +0300

    simulates fifo replacement policy

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""1-fifo_cache module."""
+
+
+BaseCaching = __import__('baseClass').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache class."""
+
+    def __init__(self):
+        """Initialize."""
+        super().__init__()
+        self.cache_lines = []
+
+    def put(self, key, item):
+        """Assign to the dictionary an item for the key.
+
+        Args:
+            key (str): key
+            item (str): item
+
+        Returns:
+            None
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(list(self.cache_data.keys())[0]))
+            self.cache_data.pop(self.cache_lines[0])
+            del self.cache_lines[0]
+        self.cache_data[key] = item
+        self.cache_lines.append(key)
+
+    def get(self, key):
+        """Get an item by key.
+
+        Args:
+            key (str): key
+
+        Returns:
+            item (str): item None otherwise
+        """
+
+        if key is None or not (key in self.cache_data):
+            return None
+
+        return self.cache_data[key]
