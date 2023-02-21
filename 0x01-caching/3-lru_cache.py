3ba0bd386daa95735a6298e4b124025a44eafa3c
Chalo1996 <emusyoka759@gmail.com>
2023-02-21 22:37:23 +0300

    implements LRU replacement algorithm

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -3,7 +3,7 @@
 
 
 # Import the BaseCaching class from baseClass.py
-BaseCaching = __import__('baseClass').BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
