e93f5e5a08c4df80ce5e21b589b2933cf60ad18d
Chalo1996 <emusyoka759@gmail.com>
2023-02-21 20:49:45 +0300

    base class for my cache classes

--- 0x01-caching/baseClass.py
+++ 0x01-caching/baseClass.py
@@ -2,29 +2,32 @@
 """The Base Class for all the classes in the project."""
 
 
+from typing import Dict
+
+
 class BaseCaching():
     """BaseCaching defines:
         - constants of the caching system.
         - data stored in a dictionary.
     """
-    MAX_ITEMS = 4
+    MAX_ITEMS: int = 4
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize the data."""
-        self.cache_data = {}
+        self.cache_data: Dict[str, str] = {}
 
-    def print_cache(self):
+    def print_cache(self) -> None:
         """Print the cache."""
         print("Current cache:")
         for key in sorted(self.cache_data.keys()):
             print("{}: {}".format(key, self.cache_data.get(key)))
 
-    def put(self, key, item):
+    def put(self, key: str, item: str) -> None:
         """Add an item in the cache."""
         raise NotImplementedError(
             "put must be implemented in your cache class")
 
-    def get(self, key):
+    def get(self, key: str) -> None:
         """Get an item by key."""
         raise NotImplementedError(
             "get must be implemented in your cache class")
