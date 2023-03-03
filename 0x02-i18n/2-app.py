e2d76d4945f348aeaba8c5c65ee48f24392d3f0d
Chalo1996 <emusyoka759@gmail.com>
2023-03-03 19:42:22 +0300

    Locale selector

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""2-app module."""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get_locale."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """index."""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
