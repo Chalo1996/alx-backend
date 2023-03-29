db0a53d925a176b333a8bf4a307d82e3fafeb6c3
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-29 23:52:41 +0300

    Promisifies data querying from the redis server using async / await

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,33 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server: ',
+    err.message);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool (schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue (schoolName) {
+  const resAsync = promisify(client.get).bind(client);
+  try {
+    const value = await resAsync(schoolName);
+    console.log(`Value: ${value}`);
+  } catch (e) {
+    console.log(`Error: ${e}`);
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+setNewSchool('Chala', 'Chala Secondary');
+displaySchoolValue('Chala');
